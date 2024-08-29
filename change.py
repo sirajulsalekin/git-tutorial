c77e7d41b67e17df3f5e96696d707fa33e0fb56d
Sirajul Salekin <salekin_eee@yahoo.com>
2024-08-29 11:59:23 -0700

    Version 1

---
+++ change.py
@@ -0,0 +1,9 @@
+# Define two numbers
+num1 = 10
+num2 = 20
+
+# Add the two numbers
+multi = num1 * num2
+
+# Print the result
+print("The multiplication of", num1, "and", num2, "is", multi)
---
+++ src/subtract.py
@@ -0,0 +1,9 @@
+# Define two numbers
+num1 = 10
+num2 = 20
+
+# Add the two numbers
+result = num1 - num2
+
+# Print the result
+print("The difference of", num1, "and", num2, "is", result)
